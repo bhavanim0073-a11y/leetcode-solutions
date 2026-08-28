0e2fc3b50ca4100cbef3d8aaea22b26eff41ad46
bhavanim0073-a11y <bhavanim0073@gmail.com>
2026-08-29 00:05:36 +0530

    Add solution for LeetCode 20

---
+++ 20-valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution(object):
+
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+
+        pairs = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for ch in s:
+            if ch in pairs:
+                if not stack:
+                    return False
+                if stack.pop() != pairs[ch]:
+                    return False
+
+            else:
+                stack.append(ch)
+        return len(stack) == 0
