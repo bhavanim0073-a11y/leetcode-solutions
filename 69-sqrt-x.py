2b183776e8cbacef3dcb13a0033a47c7e1f01b65
bhavanim0073-a11y <bhavanim0073@gmail.com>
2026-08-31 21:38:58 +0530

    Add solution for LeetCode 69

---
+++ 69-sqrt-x.py
@@ -0,0 +1,29 @@
+class Solution(object):
+
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        if x < 2:
+            return x
+
+        left = 1
+        right = x // 2
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            if mid * mid == x:
+                return mid
+
+            elif mid * mid < x:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        # right is the integer square root
+        return right
