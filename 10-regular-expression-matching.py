113e6813c0e7f3d24a9de1362cb2bebd8090596c
bhavanim0073-a11y <bhavanim0073@gmail.com>
2026-08-31 21:45:33 +0530

    Add solution for LeetCode 10

---
+++ 10-regular-expression-matching.py
@@ -0,0 +1,44 @@
+class Solution(object):
+
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+
+        m = len(s)
+        n = len(p)
+
+        # dp[i][j] means:
+        # first i characters of s match first j characters of p
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+
+        # Empty string matches empty pattern
+        dp[0][0] = True
+
+        # Handle patterns like a*, a*b*, a*b*c*
+        for j in range(2, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+
+        # Fill the DP table
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+
+                # Current characters match
+                # or pattern has '.'
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+
+                # '*' case
+                elif p[j - 1] == '*':
+                    
+                    # Case 1: '*' matches zero occurrences
+                    dp[i][j] = dp[i][j - 2]
+
+                    # Case 2: '*' matches one or more occurrences
+                    if p[j - 2] == '.' or p[j - 2] == s[i - 1]:
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+
+        return dp[m][n]
